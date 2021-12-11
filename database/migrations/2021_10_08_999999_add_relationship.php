144344b40109e6f7153f17d5ed9130de4e288bcc
duane <duane@elangaming.com>
2021-12-11 22:15:37 +0700

    update migrate

--- database/migrations/2021_10_08_999999_add_relationship.php
+++ database/migrations/2021_10_08_999999_add_relationship.php
@@ -22,11 +22,11 @@ class AddRelationship extends Migration
            $table->foreign('brand_id')->references('id')->on('brand')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('collection_store', function (Blueprint $table) {
-           $table->foreign('store_id')->references('id')->on('store');
-           $table->foreign('user_id')->references('id')->on('user');
+           $table->foreign('store_id')->references('id')->on('store')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('coupon', function (Blueprint $table) {
-           $table->foreign('store_id')->references('id')->on('store');
+           $table->foreign('store_id')->references('id')->on('store')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('order', function (Blueprint $table) {
            $table->foreign('coupon_id')->references('id')->on('coupon');
@@ -38,28 +38,28 @@ class AddRelationship extends Migration
            $table->foreign('product_id')->references('id')->on('product');
        });
        Schema::table('posts', function (Blueprint $table) {
-           $table->foreign('user_id')->references('id')->on('user');
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('comment_rating', function (Blueprint $table) {
-           $table->foreign('user_id')->references('id')->on('user');
-           $table->foreign('product_id')->references('id')->on('product');
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade')->onUpdate('cascade');
            $table->foreign('parent_id')->references('id')->on('comment_rating');
        });
        Schema::table('comment_post', function (Blueprint $table) {
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
            $table->foreign('parent_id')->references('id')->on('comment_post');
-           $table->foreign('user_id')->references('id')->on('user');
-           $table->foreign('post_id')->references('id')->on('posts');
        });
        Schema::table('rooms', function (Blueprint $table) {
-           $table->foreign('store_id')->references('id')->on('store');
-           $table->foreign('user_id')->references('id')->on('user');
+           $table->foreign('store_id')->references('id')->on('store')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('message', function (Blueprint $table) {
-           $table->foreign('room_id')->references('id')->on('rooms');
-           $table->foreign('user_id')->references('id')->on('user');
+           $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('attribute_product', function (Blueprint $table) {
-           $table->foreign('product_id')->references('id')->on('product');
+           $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade')->onUpdate('cascade');
        });
        Schema::table('thumbs_up_posts', function (Blueprint $table) {
         $table->foreign('user_id')->references('id')->on('user');
